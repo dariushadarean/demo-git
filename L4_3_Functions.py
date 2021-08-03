6565852e09d53d428004efe35771f346552911ff
Darius Hadarean <darius.hadarean.m@gmail.com>
2021-08-03 16:42:58 +0300

    First commit

---
+++ L4_3_Functions.py
@@ -0,0 +1,10 @@
+def cleanup(string):
+    """Sterge spatiile de la inceput si sfarsit
+    Transforma stringul in formatul prima litera mare, urmatoarele mici
+    Retuneaza stringul
+    """
+    new_string = string.strip()
+    new_string = new_string.capitalize()
+    return new_string
+
+print(cleanup("   text de transformat "))
